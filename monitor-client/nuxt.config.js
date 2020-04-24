58c7e0d300837fb2c61abd3054bb0b4816a6f3fc
uuhnaut69 <uuhnaut96@gmail.com>
2020-04-24 18:37:21 +0700

    Add temp client

--- monitor-client/nuxt.config.js
+++ monitor-client/nuxt.config.js
@@ -1,65 +1,62 @@
-
 export default {
   mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || 'Monitoring Debezium',
+    title: 'Monitoring Debezium',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
   /*
-  ** Global CSS
-  */
-  css: [
-    'vuesax/dist/vuesax.css'
-  ],
+   ** Global CSS
+   */
+  css: ['vuesax/dist/vuesax.css'],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/vuesax'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['@/plugins/vuesax'],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
+    '@nuxtjs/dotenv'
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {
+    baseURL: 'http://localhost:8080'
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
--- monitor-client/pages/index.vue
+++ monitor-client/pages/index.vue
@@ -1,134 +1,65 @@
 <template>
-  <div class="container">
-    <div>
-      <header class="content-logos">
-        <logo />
-        <span class="plus">+</span>
-        <VuesaxLogo />
-      </header>
-      <h1 class="title">
-        Nuxt.js + Vuesax
-      </h1>
-      <h2 class="subtitle">
-        <a href="https://vuesax.com/">Vuesax</a> is a framework of ui components for <a href="https://vuejs.org/">Vuejs</a>, It was created to make new interfaces that have a new trend and are visually beautiful
-      </h2>
-      <div class="links">
-        <h3 class="h3">
-          Vuesax
-        </h3>
-        <a
-          href="https://vuesax.com/"
-          target="_blank"
-          class="button--green"
+  <div class="center grid">
+    <vs-row>
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" w="12">
+        <img src="~/static/dbz.png" width="15%" height="15%" />
+      </vs-col>
+    </vs-row>
+    <vs-row>
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" w="3">
+        <vs-button
+          success
+          flat
+          :active="action == 'start'"
+          @click="triggerCdc('start')"
         >
-          Documentation
-        </a>
-        <a
-          href="https://discordapp.com/invite/9dsKtvB"
-          target="_blank"
-          class="button--grey"
+          Start
+        </vs-button>
+      </vs-col>
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" w="3">
+        <vs-button
+          danger
+          flat
+          :active="action == 'stop'"
+          @click="triggerCdc('stop')"
         >
-          Discord
-        </a>
-        <a
-          href="https://github.com/lusaxweb/vuesax"
-          target="_blank"
-          class="button--grey"
+          Stop
+        </vs-button>
+      </vs-col>
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" w="3">
+        <vs-button
+          warn
+          flat
+          :active="action == 'reset'"
+          @click="triggerCdc('reset')"
         >
-          GitHub
-        </a>
-      </div>
-      <div class="links">
-        <h3 class="h3">
-          Nuxt.js
-        </h3>
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+          Reset offset
+        </vs-button>
+      </vs-col>
+    </vs-row>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuesaxLogo from '~/components/VuesaxLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuesaxLogo
+  components: {},
+  data: () => ({
+    action: ''
+  }),
+  methods: {
+    async triggerCdc(event) {
+      const loading = this.$vs.loading()
+      this.action = event
+      const result = await this.$axios.$post(`/sync/${this.action}`)
+      console.log(result)
+      loading.close()
+    }
   }
 }
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 55px;
-  color: #35495e;
-  letter-spacing: 1px;
-  text-transform: capitalize;
-  margin: 25px 0px
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 1.1rem;
-  color: #526488;
-  word-spacing: 2px;
-  padding-bottom: 15px;
-  max-width: 600px;
-}
-
-.subtitle a {
-  font-weight: 500;
-  color: inherit
-}
-
-.links {
-  padding-top: 15px;
-  margin-bottom: 20px
-}
-
-.content-logos {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-width: 500px;
-}
-.plus {
-  font-size: 2.5rem;
-  margin: 15px;
-  color: #35495e
-}
-.h3 {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-weight: 400;
-  margin: 10px;
+  background: black;
 }
 </style>
