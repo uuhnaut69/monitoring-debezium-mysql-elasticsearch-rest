309d926f5020cdf0b355f7de03b791dbd2b0911e
uuhnaut69 <uuhnaut96@gmail.com>
2020-05-16 08:13:13 +0700

    Add update/delete endpoint

--- api/src/main/java/com/uuhnaut69/api/rest/CompanyRest.java
+++ api/src/main/java/com/uuhnaut69/api/rest/CompanyRest.java
@@ -1,11 +1,10 @@
 package com.uuhnaut69.api.rest;
 
 import com.uuhnaut69.api.model.Company;
+import com.uuhnaut69.api.payload.request.CompanyRequest;
 import com.uuhnaut69.api.service.CompanyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +23,15 @@ public class CompanyRest {
     public List<Company> dummyData() {
         return companyService.dummyData();
     }
+
+    @PutMapping("/{companyId}")
+    public Company update(@PathVariable Long companyId, @RequestBody CompanyRequest companyRequest) {
+        return companyService.update(companyId, companyRequest);
+    }
+
+    @DeleteMapping("/{companyId}")
+    public void delete(@PathVariable Long companyId) {
+        companyService.deleteById(companyId);
+    }
+
 }
